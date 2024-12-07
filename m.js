3acab4ffd7a716e511aa9368a3a0f463a0308da8
Aniket Dobriyal <126316970+aniketdobriyal@users.noreply.github.com>
2024-12-07 18:15:02 +0530

    Update m.js

--- m.js
+++ m.js
@@ -12,20 +12,20 @@ function chng() {
     x.style.animation = null;
 
     if (n == 1) {
-        x.style = "background-image:url('https://img2.juzaphoto.com/001/shared_files/uploads_hr/1465319_large5419.jpg');";
+        x.style = "background-image:url('1465319_large5419.jpg');";
         document.getElementById("titles").innerHTML = "<b>SPECIAL</b> <br>OFFERS";
 
     }
 
     else if (n == 2) {
-        x.style = "background-image:url('https://wallup.net/wp-content/uploads/2016/05/13/338926-women-tattoos.jpg')";
+        x.style = "background-image:url('338926-women-tattoos.jpg')";
         document.getElementById("titles").innerHTML = "<b>DISCOUNTS <br>FOR FIRST-TIMER </b>";
 
     }
 
 
     else if (n == 3) {
-        x.style = "background-image:url('https://das-tattoostudio-hannover.de/wp-content/uploads/2022/01/tattoostudio-hannover-blog-01-1536x864.jpg')";
+        x.style = "background-image:url('AdobeStock_291190502-scaled.jpeg')";
         n = 0;
         document.getElementById("titles").innerHTML = "<b>BOOK NOW</b> <br>";
 
